246ccd90cdadd79b9b6c35df1b1ee4b2eab0c03f
rahulrathod6203 <rahulrathod6203@gmail.com>
2026-08-05 14:45:59 +0530

    first additon program added

---
+++ unit-testing/src/main/java/com/cg/coding/AdditionDemo.java
@@ -0,0 +1,13 @@
+package com.cg.coding;
+
+public class AdditionDemo {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result  = add(10, 10);
+        System.out.println(result);
+    }
+}
