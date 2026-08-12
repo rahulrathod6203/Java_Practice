9cf36e7e760395fd39c1ee8b84ccee2f10535402
rahulrathod6203 <rahulrathod6203@gmail.com>
2026-08-12 14:26:00 +0530

    BankAccountValidator Challenge for writing Unit tests completed and all test cases implemented uing TDD approach

--- unit-testing/src/main/java/com/cg/tdd/practice/challenge/BankAccountValidator.java
+++ unit-testing/src/main/java/com/cg/tdd/practice/challenge/BankAccountValidator.java
@@ -4,4 +4,27 @@ public class BankAccountValidator {
     boolean isValidAccountHolderName(String name) {
         return name.length() >= 3 && name.length() <= 20;
     }
+
+    boolean isValidAge(int age) {
+        return age >= 18 && age <= 60;
+    }
+
+    boolean isValidAccountNumber(String accountNumber) {
+        if (accountNumber == null || accountNumber.length() != 10) {
+            return false;
+        }
+
+        for (char ch : accountNumber.toCharArray()) {
+            if (!Character.isDigit(ch)) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    boolean isBlank(String value){
+        return value ==null || value.isBlank();
+    }
 }
--- unit-testing/src/test/java/com/cg/tdd/practice/challenge/BankAccountValidatorTest.java
+++ unit-testing/src/test/java/com/cg/tdd/practice/challenge/BankAccountValidatorTest.java
@@ -1,31 +1,73 @@
 package com.cg.tdd.practice.challenge;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BankAccountValidatorTest {
 
     BankAccountValidator accountValidator;
 
     @BeforeEach
-    void init(){
-        accountValidator =  new BankAccountValidator();
+    void init() {
+        accountValidator = new BankAccountValidator();
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"Rahul","John Doe","Priya123"})
-    void shouldTestValidAccountHolderName(String name){
+    @ValueSource(strings = {"Rahul", "John Doe", "Priya123"})
+    void shouldTestValidAccountHolderName(String name) {
         assertTrue(accountValidator.isValidAccountHolderName(name));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"Ra","ABCDEFGHIJKLMNOPQRSTUVWXYZ"})
-    void shouldTestInValidAccountHolderName(String name){
+    @ValueSource(strings = {"Ra", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"})
+    void shouldTestInValidAccountHolderName(String name) {
         assertFalse(accountValidator.isValidAccountHolderName(name));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "18,true",
+            "17,false",
+            "16,false",
+            "60,true",
+            "61,false"
+    })
+    void shouldValidateAge(int age, boolean expected) {
+        assertEquals(expected, accountValidator.isValidAge(age));
+    }
+
+    public static Stream<Arguments> accountNumberTestData() {
+        return Stream.of(
+                Arguments.of("1234567890", true),
+                Arguments.of("12345", false),
+                Arguments.of(null, false),
+                Arguments.of("123456789A", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("accountNumberTestData")
+    void shouldValidateAccountNumber(String accountNumber, boolean expected) {
+        assertEquals(expected, accountValidator.isValidAccountNumber(accountNumber));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = { "  ", "\t", "\n" })
+    void shouldValidateEmptyAndNullValues(String value) {
+        assertTrue(accountValidator.isBlank(value));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"hello", "hi"})
+    void shouldValidateNotEmptyOrNonNullValues(String value) {
+        assertFalse(accountValidator.isBlank(value));
     }
 
 }
